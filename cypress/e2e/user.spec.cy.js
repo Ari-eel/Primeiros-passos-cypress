cca7406ed9f8f99e6a679c3b36959c35349be796
Ariel Sales <arieelsales@gmail.com>
2025-06-09 21:07:56 -0300

    User updated adding

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -15,7 +15,7 @@ describe ('Orange HRM Tests', () => {
     genericField: ".oxd-input--active",
     dateField: ".oxd-date-input",
     submitButton: ".orangehrm-left-space[type='submit']",
-    countryButton: ".oxd-select-text--active",
+    comboboxButton: ".oxd-select-text--active",
   }
 
   it.only ('User Info Update - sucess', () => {
@@ -33,13 +33,13 @@ describe ('Orange HRM Tests', () => {
     cy.get(selectorsList.genericField).eq(5).clear().type('DriversLicenseTest')
     cy.get(selectorsList.dateField).eq(0).clear().click().type('2030-12-02')
     cy.get(selectorsList.dateField).eq(1).clear().click().type('1999-05-12')
+    cy.get(selectorsList.comboboxButton).eq(0).click()
+    cy.get('.oxd-select-dropdown').contains('Brazilian').click()
+    cy.get(selectorsList.comboboxButton).eq(1).click()
+    cy.get('.oxd-select-dropdown').contains('Single').click()
     cy.get(selectorsList.submitButton).eq(0).click()
     cy.get(selectorsList.submitButton).eq(1).click()
     cy.get('body').should('contain','Successfully Updated')
-    cy.get(selectorsList.countryButton).eq(0).click()
-    cy.get('.oxd-select-dropdown').contains('Brazilian').click()
-    cy.get(selectorsList.countryButton).eq(1).click()
-    cy.get('.oxd-select-dropdown').contains('Single').click()
 
   })
 
